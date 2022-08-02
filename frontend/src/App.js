ed2f2695925e1ab3ddf7001a6044e2501231ce6a
Christopher Fong <christopherjayfong@gmail.com>
2022-08-02 12:32:49 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
+// import React from 'react';
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import LoginPage from './pages/LoginPage';
+// import SignUpPage from './pages/SignUpPage';
+// import VerifyEmailPage from './pages/VerifyEmailPage';
+// import ForgotPasswordPage from './pages/ForgotPasswordPage';
+// import ResetPasswordPage from './pages/ResetPasswordPage';
+// import HomePage from './pages/HomePage';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" index element={<LoginPage />} />
+//         <Route path="/signup" index element={<SignUpPage />} />
+//         <Route path="/verifyemail" index element={<VerifyEmailPage />} />
+//         <Route path="/forgotpassword" index element={<ForgotPasswordPage />} />
+//         <Route path="/resetpassword" index element={<ResetPasswordPage />} />
+//         <Route path="/home" index element={<HomePage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import VerifyEmailPage from './pages/VerifyEmailPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
 import HomePage from './pages/HomePage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={<LoginPage />} />
-        <Route path="/signup" index element={<SignUpPage />} />
-        <Route path="/verifyemail" index element={<VerifyEmailPage />} />
-        <Route path="/forgotpassword" index element={<ForgotPasswordPage />} />
-        <Route path="/resetpassword" index element={<ResetPasswordPage />} />
-        <Route path="/home" index element={<HomePage />} />
-      </Routes>
+        <Route path="/" index element={<HomePage />} />
+        </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Home from '../components/Home';
-import PageTitle from '../components/PageTitle';
+import TopHome from '../components/TopHome';
 
 const HomePage = () =>
 {
 
     return(
       <div id="page3">
-        <PageTitle />
+        <TopHome />
         <Home />
       </div>
     );
